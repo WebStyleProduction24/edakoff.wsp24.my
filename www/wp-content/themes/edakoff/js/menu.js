7513d30d29dcc183788fe8f844e3ac67f999e929
Jack36vrn <Jack36@list.ru>
2018-04-13 23:27:23 +0300

    v.0.0.7

--- www/wp-content/themes/edakoff/js/menu.js
+++ www/wp-content/themes/edakoff/js/menu.js
@@ -1,11 +1,16 @@
 $(function() {
 	var pull 		= $('#pull');
 	menu 		= $('nav ul');
+	logo		= $('.logo');
+	phone		= $('.phone');
 	menuHeight	= menu.height();
+	logoHeight	= logo.height();
+	phoneHeight	= phone.height();
 
 	$(pull).on('click', function(e) {
 		e.preventDefault();
 		menu.slideToggle();
+		logo.css('float', 'none');
 	});
 
 	$(window).resize(function(){
