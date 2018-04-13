928947861b79d554e16cce3840515ba29474b1f3
Jack36VRN <36456429+Jack36VRN@users.noreply.github.com>
2018-04-13 09:51:54 +0300

    v.0.0.3
    Добавлено меню. Необходимо его отладить

--- www/wp-content/themes/edakoff/functions.php
+++ www/wp-content/themes/edakoff/functions.php
@@ -4,6 +4,9 @@ function enqueue_styles() {
 	wp_enqueue_style( 'whitesquare-style', get_stylesheet_uri());
 	wp_register_style('font-style', 'http://fonts.googleapis.com/css?family=Oswald:400,300');
 	wp_enqueue_style( 'font-style');
+	wp_register_style('reset', get_template_directory_uri().'/css/reset.css');
+	wp_enqueue_style( 'reset');
+
 }
 add_action('wp_enqueue_scripts', 'enqueue_styles');
 
@@ -11,6 +14,8 @@ function my_scripts_method() {
     wp_deregister_script( 'jquery' );
     wp_register_script( 'jquery', 'https://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js');
     wp_enqueue_script( 'jquery' );
+    wp_register_script( 'menu', get_template_directory_uri().'/js/menu.js');
+    wp_enqueue_script( 'menu' );
 }    
  
 add_action( 'wp_enqueue_scripts', 'my_scripts_method' );
