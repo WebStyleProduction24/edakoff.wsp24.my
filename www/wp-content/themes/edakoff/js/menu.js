f6347cadfc2e390a7eca1af029790ffa6bbb078a
Jack36vrn <Jack36@list.ru>
2018-04-14 09:18:17 +0300

    Revert "v.0.0.7"
    This reverts commit 7513d30d29dcc183788fe8f844e3ac67f999e929.

--- www/wp-content/themes/edakoff/js/menu.js
+++ www/wp-content/themes/edakoff/js/menu.js
@@ -1,16 +1,11 @@
 $(function() {
 	var pull 		= $('#pull');
 	menu 		= $('nav ul');
-	logo		= $('.logo');
-	phone		= $('.phone');
 	menuHeight	= menu.height();
-	logoHeight	= logo.height();
-	phoneHeight	= phone.height();
 
 	$(pull).on('click', function(e) {
 		e.preventDefault();
 		menu.slideToggle();
-		logo.css('float', 'none');
 	});
 
 	$(window).resize(function(){
